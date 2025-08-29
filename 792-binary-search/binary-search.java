80da4c3612a41368900b14c6065896917cb29fb9
Hariom Maurya <hari08756om@gmail.com>
2025-08-29 17:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (61.15%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,5 +1,6 @@
 class Solution {
-    int binarySearch(int [] nums, int n, int target){
+    int binarySearch(int [] nums, int target){
+        int n = nums.length;
         int l = 0;
         int h = n - 1;
         int mid = 0 ;
@@ -13,7 +14,6 @@ class Solution {
         else return -1;
     }
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        return binarySearch(nums , n, target);        
+        return binarySearch(nums, target);        
     }
 }
