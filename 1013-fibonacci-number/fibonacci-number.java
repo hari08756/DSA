9e4934dbb69fd7329874d30ab2e8b239adc0eb2d
Hariom Maurya <hari08756om@gmail.com>
2025-09-07 07:19:29 +0530

    Time: 8 ms (45.23%) | Memory: 40.6 MB (38.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,7 @@
 class Solution {
     int fibo(int n){
         int fibn =0;
-        if(n<=0) return 0;
-        else if(n==1) return 1;
+        if(n<2) return n;
         else {
             fibn = fibo(n-1) + fibo(n-2);
         }
