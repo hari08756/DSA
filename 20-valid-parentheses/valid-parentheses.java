5bcd7ae398396aff7fe6204395d59acf401585ce
Hariom Maurya <hari08756om@gmail.com>
2025-11-19 12:37:37 +0530

    Time: 3 ms (66.73%) | Memory: 43.4 MB (12.37%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,41 @@
 class Solution {
-    public char getVal(char ch){
-        switch(ch){
-            case']' : return '[';
-            case'}' : return '{';
-            case')' : return '(';
-            default : return ' ';
+    boolean isValidString(Stack<Character> st, char c){
+        if(c == '(' || c == '[' || c == '{'){
+            st.push(c);
+            return true;
         }
+        if(st.size() == 0) return false;
+        if(c == ')'){
+            if(st.peek() == '('){
+                st.pop();
+                return true;
+            }
+        }
+        if(c == ']'){
+            if(st.peek() == '['){
+                st.pop();
+                return true;
+            }
+        }
+        if(c == '}'){
+            if(st.peek() == '{'){
+                st.pop();
+                return true;
+            }
+        }
+        return false;
     }
     public boolean isValid(String s) {
-        String opening = "({[";
-        String closing = "]})";
-        Stack<Character> st=new Stack<>();
-        for (char ch:s.toCharArray())
-        {
-            if(opening.indexOf(ch)!=-1)
-               st.push(ch);
-            else{
-                if(st.size()==0) return false;
-                char tmp= st.pop();
-                if(getVal(ch)!=tmp)
-                return false ;
-            }   
-        } 
-        return st.size()==0 ;// return
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        if(n%2 != 0) return false;
+        for(int i = 0; i<n; i++){
+            char c = s.charAt(i);
+            if(!isValidString(st,c)){
+                return false;
+            }
+        }
+        if(st.size() != 0) return false;
+        return true;
     }
 }
