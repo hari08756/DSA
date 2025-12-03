5b8b571e6606d3cb857ea859b8e0709b86b6c721
Hariom Maurya <hari08756om@gmail.com>
2025-12-03 13:38:06 +0530

    Time: 14 ms (51.13%) | Memory: 51.6 MB (5.77%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -4,14 +4,15 @@ class Solution {
         if(s.length == 0) return 0;
         Arrays.sort(g);
         Arrays.sort(s);
-        int maxChild = 0;
-        int j = s.length -1;
-        for(int i = g.length -1; i>=0; i--){
-            if(j>=0 && g[i] <= s[j]){
-                maxChild ++;
-                j--;
-            }  
+        int n = g.length , m = s.length;
+        int l = 0, r = 0;
+        while(l<m && r<n){
+            if(g[r] <= s[l]){
+                r++;
+            }
+            l++;
         }
-        return maxChild;
+        
+        return r;
     }
 }
