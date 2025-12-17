56fa7bb686d7c3051d45f420a621978cac28c3b3
Hariom Maurya <hari08756om@gmail.com>
2025-12-17 09:42:16 +0530

    Time: 4 ms (71.59%) | Memory: 44.3 MB (48.52%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,41 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
-        if (root != null) {
-            dfs(root, "", paths);
-        }
-        return paths;
-    }
-
-    private void dfs(TreeNode node, String path, List<String> paths) {
-        if (node.left == null && node.right == null) {
-            paths.add(path + node.val);
+class Solution {
+    public void helper(TreeNode root, List<String> ans, String s){
+        if(root == null) return;
+        if(root.left == null && root.right == null){
+            s += root.val;
+            ans.add(s);
             return;
         }
-        String currentPath = path + node.val + "->";
-        if (node.left != null) {
-            dfs(node.left, currentPath, paths);
-        }
-        if (node.right != null) {
-            dfs(node.right, currentPath, paths);
-        }
+        helper(root.left, ans, s+root.val +"->");
+        helper(root.right, ans, s+root.val +"->");
+
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        helper(root,ans,"");
+        return ans;
     }
 }
