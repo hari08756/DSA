61fa5b32142b3267d45e03a2ce51f3d3003a2475
Hariom Maurya <hari08756om@gmail.com>
2025-12-22 15:55:45 +0530

    Time: 18 ms (51.24%) | Memory: 48.7 MB (48.31%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -1,16 +1,52 @@
 class Solution {
-    public void getElement(TreeNode root, List<Integer> ans){
+    public void morrisTrav(TreeNode root, List<Integer> list){
         if(root == null) return;
-        getElement(root.left, ans);
-        ans.add(root.val);
-        getElement(root.right, ans);
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr){
+                    pred = pred.right;
+                }
+                if(pred.right == null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }else{
+                    pred.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
+                }
+            }else{
+                list.add(curr.val);
+                curr = curr.right;
+            }
+        }
+    }
+    public void merge(List<Integer> l1, List<Integer> l2, List<Integer> ans){
+        int i = 0, j = 0;
+        int n = l1.size(), m = l2.size();
+        while(i<n && j<m){
+            if(l1.get(i)<l2.get(j)){
+                ans.add(l1.get(i++));
+            }else{
+                ans.add(l2.get(j++));
+            }
+        }
+        while(i<n){
+            ans.add(l1.get(i++));
+        }
+        while(j<m){
+            ans.add(l2.get(j++));
+        }
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        if(root1 == null && root2 == null) return l1;
+        morrisTrav(root1, l1);
+        morrisTrav(root2, l2);
         List<Integer> ans = new ArrayList<>();
-        if(root1 == null && root2 == null) return ans;
-        getElement(root1, ans);
-        getElement(root2, ans);
-        Collections.sort(ans);
+        merge(l1, l2, ans);
         return ans;
     }
 }
