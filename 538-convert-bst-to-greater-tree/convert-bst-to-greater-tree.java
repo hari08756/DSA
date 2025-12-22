dd25082ed769c95d51d101fd48a7a6a7eb7b3a9c
Hariom Maurya <hari08756om@gmail.com>
2025-12-22 10:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (13.68%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -1,12 +1,11 @@
 class Solution {
     public static int sum = 0;
-    public TreeNode maxBST(TreeNode root){
-        if(root == null) return  null ;
+    public void maxBST(TreeNode root){
+        if(root == null) return ;
         maxBST(root.right);
         sum += root.val;
         root.val = sum;
         maxBST(root.left);
-        return root;
     }
     public TreeNode convertBST(TreeNode root) {
         sum = 0;
